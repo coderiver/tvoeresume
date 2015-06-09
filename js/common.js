c2e298aed542333496d852301f17efc7c2f850a9
etna-scorpio <etna.scorpio@gmail.com>
2015-06-09 11:34:18 +0300

    main done

--- js/common.js
+++ js/common.js
@@ -4,20 +4,36 @@ head.ready(function() {
 	// 	$(".js-popup").hide();
 	// });
 
-	// function scrollFixedElements() {
-	//     var scroll_left = $(this).scrollLeft();
-	//     $(".fixed-element").css({
-	//         left: -scroll_left
-	//     });
-	// }
-	// scrollFixedElements();
-	// $(window).scroll(function(){
-	//     scrollFixedElements()
-	// });
-
 	$('.js-sign').click(function(e){
 		e.preventDefault;
 		$(this).closest('.header__wrap').find('.js-form').toggleClass('is-visible');
+	});
+
+	$('.js-hover').on('mouseenter', function () {
+		$(this).parent().children('.js-hover').find('.character__img').addClass('is-small');
+		$(this).find('.character__img').removeClass('is-small');
+	});
+
+	$('.js-hover').on('mouseleave', function () {
+		$(this).parent().children('.js-hover').find('.character__img').removeClass('is-small');
+	});
+
+	$('.radiobox input').on('change', function () {
+
+		var container = $('.content');
+
+		if($(this).val() == 'boy'){
+			container.removeClass('right-grey');
+			container.addClass('left-grey');
+		}
+		if($(this).val() == 'girl'){
+			container.removeClass('left-grey');
+			container.addClass('right-grey');
+		}
+		if($(this).val() == 'teacher'){
+			container.removeClass('left-grey');
+			container.removeClass('right-grey');
+		}
 	});
 
 });
